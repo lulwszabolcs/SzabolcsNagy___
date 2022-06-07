4bf71dcd12866bf4154b39547a23517e09615788
Szabolcs Nagy <48963820+lulwszabolcs@users.noreply.github.com>
2022-06-07 13:48:24 +0200

    Add files via upload

---
+++ 06.07.py
@@ -0,0 +1,12 @@
+#1
+#kérj be nevet és életkort 2x ki a fiatalabb vagy egyidős
+#3 számot kérj be vizsgáld meg hogy bármely 2 szám összege egyenlő a 3. számmal
+#kérj be 2 szót és irasd ki hogy melyik a hosszabb vagya rövidebb vagy egyforma -e a két szó
+#2
+#lista véletlen számokkal függvény számok osztóinak száma
+#prímszámok
+#2 számjegyű-e vagy sem
+#3
+#txt beolvasás   ; vagy szóköz számokat, fileba iratás, monitorra kiíratás
+#monitoroknál átlag nagyság 2 tizedesjegyre
+#régi film új film,kicsi nagy
